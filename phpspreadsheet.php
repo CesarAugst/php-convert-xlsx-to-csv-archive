e2f820353c05786adf18df36aaf90033f05cba51
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-04-03 13:27:45 -0300

    divisao por modulos

--- phpspreadsheet.php
+++ phpspreadsheet.php
@@ -1,12 +1,15 @@
 <?php
 //necessario para utilizacao do composer
 require_once 'vendor/autoload.php';
+//modulos proprios
+require_once 'modules/create_new_csv_archive.php'; //criacao de arquivos csv
+require_once 'modules/spreadsheet_read_file.php'; //leitura de arquivos usando spreadsheet
 
 //pega nome do arquivo
 $file_name = "files/p3.xls";
 
 //chama leitura do arquivo
-$xlsx_content = safe_read_file($file_name);
+$xlsx_content = spreadsheet_read_file($file_name);
 //se houver conteudo
 if($xlsx_content){
     //chama a criacao de novo arquivo
@@ -14,52 +17,4 @@ if($xlsx_content){
 }else{
     //exibe mensagem de erro
     echo "nao foi possivel realizar a conversao. Dica: verifique a extensao do arquivo";
-}
-
-//desc: faz criacao de arquivo csv com base nos dados do arquivo xlsx recebido
-//params: (string) nome do arquivo, (array) dados recebidos
-//return: nenhum
-function create_new_csv_archive($file_name, $array_to_file_content){
-    //abre o arquivo com permissao de escrita
-    $archive = fopen("$file_name.csv", 'w');
-    //loop com os dados recebidos
-    foreach ($array_to_file_content as $row){
-        //escreve linha no arquivo separando dados por ;
-        fputcsv($archive, $row, ';');
-    }
-    //fecha o arquivo
-    fclose($archive);
-}
-
-//desc: faz a leitura de arquivo xlsx e converte em array
-//params: (string) nome do arquivo, (string)
-//return: (array) conteudo do arquivo
-function safe_read_file($file_name){
-    //separa nome do arquivo da extensao
-    $array_file_name = explode('.', $file_name);
-    //pega somente a extensao
-    $extension = end($array_file_name);
-
-    //se for do formato xlz
-    if('xls' == $extension){
-        //recebe funcao de xls para ler os dados do arquivo
-        $reader = new PhpOffice\PhpSpreadsheet\Reader\Xls();
-    //se for do formato xlsx
-    }elseif ('xlsx' == $extension) {
-        //recebe funcao de xlsx para ler os dados do arquivo
-        $reader = new PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-    }elseif('csv' == $extension){
-        //recebe funcao de csv para ler os dados do arquivo
-        $reader = new PhpOffice\PhpSpreadsheet\Reader\Csv();
-    //outros formatos nao sao validos
-    }else{
-        return null;
-    }
-    //prepara para leitura do arquivo
-    $reader = $reader->load($file_name);
-    //converte os dados em array
-    $file_content = $reader->getActiveSheet()->toArray();
-
-    //retorna o array de conteuo do arquivo
-    return $file_content;
 }
