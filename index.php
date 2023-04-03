9ae8dc73898f9979c6c11bb7bd1a939080c6aeb4
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2023-04-03 11:00:18 -0300

    criação dee arquivo csv implemenada

--- index.php
+++ index.php
@@ -1,16 +1,54 @@
 <?php
 //necessario para usar logica do composer
 require_once "vendor/autoload.php";
-//arquivo xlsx
+//arquivo xlsx para converter
 $file = "planilha-lorem.xlsx";
-//usando a biblioteca para ler
-$reader = \Ark4ne\XlReader\Factory::createReader($file);
-//prepara a leitura do arquivo
-$reader->load();
+//chama leitura do arquivo
+$xlsx_content = safe_read_file($file);
+//chama a criacao de novo arquivo
+create_new_csv_archive('planilha-lorem-convertida', $xlsx_content);
 
-echo "<pre>";
-//loop com as linhas do arquivo
-foreach ($reader->read() as $row){
-    print_r($row);
+//desc: faz criacao de arquivo csv com base nos dados do arquivo xlsx recebido
+//params: (string) nome do arquivo, (array) dados recebidos
+//return: nenhum
+function create_new_csv_archive($file_name, $array_to_file_content){
+    //abre o arquivo com permissao de escrita
+    $archive = fopen("$file_name.csv", 'w');
+    //loop com os dados recebidos
+    foreach ($array_to_file_content as $row){
+        //escreve linha no arquivo separando dados por ;
+        fputcsv($archive, $row, ';');
+    }
+    //fecha o arquivo
+    fclose($archive);
 }
-echo "</pre>";
+
+//desc: faz a leitura de arquivo xlsx e converte em array
+//params: (string) nome do arquivo, (string)
+//return: (array) conteudo do arquivo
+function safe_read_file($file, $empty_cell_indicator = "-"){
+    //inicia o conteudo do arqiuvo como array vazio
+    $file_content = [];
+
+    //usando a biblioteca para ler
+    $reader = \Ark4ne\XlReader\Factory::createReader($file);
+    //prepara a leitura do arquivo
+    $reader->load();
+
+    //pega o cabecalho
+    $header = $reader->read()->current();
+    //loop com as linhas do arquivo
+    foreach ($reader->read() as $row){
+        //inicia nova linha
+        $new_line = [];
+        //loop com os itens do cabecalho
+        foreach ($header as $key=> $field){
+            $new_line[$key] = isset($row[$key]) ? $row[$key] : $empty_cell_indicator;
+        }
+        //adiciona nova linha ao array de conteudo doa rquivo
+        $file_content[] = ($new_line);
+    }
+
+    //retorna o array de conteuo do arquivo
+    return $file_content;
+}
